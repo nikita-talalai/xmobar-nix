9ef73d05f7bc350e495a65f20548513afed2beed
Nikita Talalai <nikita.talalai.2002@gmail.com>
2024-04-30 21:53:56 +0300

    ci: fix ci errors

--- src/Main.hs
+++ src/Main.hs
@@ -3,16 +3,16 @@ import Xmobar
 config :: Config
 config =
   defaultConfig
-    { font = "DejaVu Sans Mono 15",
-      allDesktops = True,
-      alpha = 200,
-      commands =
-        [ Run $ Memory [ "-t", "Mem: <used> Gb", "--", "--scale", "1024"] 20,
-          Run $ Kbd [],
-          Run $ Date "%a %d/%m/%y <fc=#ee9a00>%H:%M</fc>" "date" 10
-        ],
-      template = "}{ %kbd% | %date% | %memory%",
-      alignSep = "}{"
+    { font = "DejaVu Sans Mono 15"
+    , allDesktops = True
+    , alpha = 200
+    , commands =
+        [ Run $ Memory ["-t", "Mem: <used> Gb", "--", "--scale", "1024"] 20
+        , Run $ Kbd []
+        , Run $ Date "%a %d/%m/%y <fc=#ee9a00>%H:%M</fc>" "date" 10
+        ]
+    , template = "}{ %kbd% | %date% | %memory%"
+    , alignSep = "}{"
     }
 
 main :: IO ()
